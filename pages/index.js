8524a51a43f46ad34ae489a631e29ff9c5717650
Vinicius Seixas <vinicius.oliveira@ccc.ufcg.edu.br>
2021-07-12 20:09:21 -0300

    initial alurakut

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,68 @@
-import styled from 'styled-components'
+import Box from '../src/components/Box'
+import MainGrid from '../src/components/MainGrid'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSidebar({ githubUser }) {
+  return (
+    <Box>
+      <img src={`https://github.com/${githubUser}.png`} style={{ borderRadius: '8px' }} />
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = 'vsseixaso'
+  const favoritePeople = [
+    'raonismaneoto',
+    'emersonllucena',
+    'gugabribeiro',
+    'joseims',
+    'peas',
+    'omariosouto',
+    'diego3g',
+    'filipedeschamps',
+    'juunegreiros',
+    'felipefialho'
+  ]
+
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar githubUser={githubUser} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1 className="title">
+              Bem vindo(a)
+            </h1>
+
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da comunidade ({favoritePeople.length})
+            </h2>
+
+            <ul>
+              {favoritePeople.map(people => {
+                return (
+                  <li>
+                    <a href={`/users/${people}`} key={people}>
+                      <img src={`https://github.com/${people}.png`} />
+                      <span>{people}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+  )
 }
